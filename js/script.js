eb734b424338b24ddb5e0cba7fd67942d91de6cb
comemaino <mat.maino@gmail.com>
2022-04-11 15:40:51 +0200

    Add: for cycle

---
+++ js/script.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 100; i++) {
+	let result;
+	if (i % 3 === 0) {
+		result = "Fizz";
+		if (i % 5 === 0) {
+			result += "Buzz";
+		}
+	} else if (i % 5 === 0) {
+		result = "Buzz";
+	} else {
+		result = i;
+	}
+	console.log(result);
+}
